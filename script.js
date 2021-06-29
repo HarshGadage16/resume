db5548df5b837814039b7b22848de4616ddc67f8
Harsh Gadage <hgadge1605@gmail.com>
2021-06-29 13:16:54 +0530

    Resume building

---
+++ script.js
@@ -0,0 +1,70 @@
+var progressBars = document.querySelectorAll(".skill-progress > div");
+
+
+
+function initialiseBar(bar) {
+    bar.setAttribute("data-visited", false);
+    bar.style.width = 0 + '%';
+}
+
+for (var bar of progressBars) {
+    initialiseBar(bar);
+}
+
+
+
+function fillBar(bar) {
+
+    var currentWidth = 0;
+    var targetWidth = bar.getAttribute("data-bar-width");
+    var interval = setInterval(function () {
+        if (currentWidth >= targetWidth) {
+            clearInterval(interval);
+            return;
+        }
+        currentWidth++;
+        bar.style.width = currentWidth + '%';
+    }, 5);
+
+}
+
+
+
+// This function uses a for loop for individual progress bars.
+function checkScroll() {
+
+    for (let bar of progressBars) {
+        var barCoordinates = bar.getBoundingClientRect();
+        if ((bar.getAttribute("data-visited") == "false") &&
+            (barCoordinates.top <= (window.innerHeight - barCoordinates.height))) {
+            bar.setAttribute("data-visited", true);
+            fillBar(bar);
+        } else if (barCoordinates.top > window.innerHeight) {
+            bar.setAttribute("data-visited", false);
+            initialiseBar(bar);
+        }
+
+    }
+}
+
+
+
+window.addEventListener("scroll", checkScroll);
+
+var navMenuAnchorTags = document.querySelectorAll('.nav-menu a');
+
+for(var i = 0; i<= navMenuAnchorTags.length; i++){
+    navMenuAnchorTags[i].addEventListener('click', function(event){
+        event.preventDefault();
+        var targetSectionId = this.textContent.trim().toLowerCase();
+        var targetSection = document.getElementById(targetSectionId);
+        var interval = setInterval(function(){
+            var targetSectionCoordintes = targetSection.getBoundingClientRect();
+            if(targetSectionCoordintes.top <= 0){
+                clearInterval(interval);
+                return;
+            }
+            window.scrollBy(0, 50);
+        }, 20);
+    });
+}
